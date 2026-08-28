bb8917f54486a4c89805de1f0bce53da04f6a85e
Zhoracio <68284759+AlbinaUshakova@users.noreply.github.com>
2026-08-28 13:32:45 +0400

    Clean stale service workers in dev

--- meal-tracker/src/main.tsx
+++ meal-tracker/src/main.tsx
@@ -3,10 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-if (import.meta.env.PROD && "serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").catch(() => undefined);
-  });
+if ("serviceWorker" in navigator) {
+  if (import.meta.env.PROD) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/service-worker.js").catch(() => undefined);
+    });
+  } else {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.getRegistrations()
+        .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+        .then(() => {
+          if (!("caches" in window)) return undefined;
+          return caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))));
+        })
+        .catch(() => undefined);
+    });
+  }
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
