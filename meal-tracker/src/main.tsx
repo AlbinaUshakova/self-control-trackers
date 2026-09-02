b8d00283ddb7c958b94aa44f5b3e0dedb93c1365
Альбина Ушакова <albina@Albinas-MacBook-Air.local>
2026-09-02 20:46:20 +0400

    Revert "Disable service worker during local development"
    This reverts commit d8438791ee3c27973525a958bfa7aa597981c24c.

--- meal-tracker/src/main.tsx
+++ meal-tracker/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-if (import.meta.env.PROD && "serviceWorker" in navigator) {
+if ("serviceWorker" in navigator && (window.location.protocol === "https:" || window.location.hostname === "localhost")) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/service-worker.js").catch(() => undefined);
   });
